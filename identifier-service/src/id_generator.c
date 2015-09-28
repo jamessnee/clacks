0413061e359f8a4acea4881d6fc03692745004f0
James Snee <james.snee@gmail.com>
2015-09-28 18:10:01 -0400

    Fix a bug in the handling of the free uuid list

--- identifier-service/src/id_generator.c
+++ identifier-service/src/id_generator.c
@@ -5,17 +5,18 @@ static uuid_t uuid_list[CL_MAX_FREE_UUIDS];
 
 void update_uuid_pool(void) {
   int i;
-  for (i = 0; i < uuid_list_hd; i++) {
+  if (uuid_list_hd < 0) uuid_list_hd = 0;
+  for (i = uuid_list_hd; i < CL_MAX_FREE_UUIDS; i++) {
     uuid_generate_time_safe(uuid_list[i]);
   }
-  uuid_list_hd = 0;
+  uuid_list_hd = CL_MAX_FREE_UUIDS;
 }
 
 void next_uuid(uuid_t uuid) {
-  if (uuid_list_hd == CL_MAX_FREE_UUIDS) {
+  if (uuid_list_hd == -1) {
     update_uuid_pool();
   }
 
   uuid = uuid_list[uuid_list_hd];
-  uuid_list_hd++;
+  uuid_list_hd--;
 }
