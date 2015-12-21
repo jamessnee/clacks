bd646e6fb3caf2f5ebea2db73b04380040ba2b07
James Snee <jas250@cam.ac.uk>
2015-12-21 16:12:57 +0000

    Added small test of join

--- test_apps/api_test.c
+++ test_apps/api_test.c
@@ -3,8 +3,9 @@
 
 int main (int argc, char *argv[]) {
 
-  char id[1024];
+  char id[37];
   int ret;
+  char j_id[37];
 
   // Test whether we can get an ID
   ret = clacks_new_id(id);
@@ -21,6 +22,17 @@ int main (int argc, char *argv[]) {
   // Test whether we can send a trace message
   fprintf(stdout, "Testing tracing\n");
   clacks_trace_string("DIFFERENT STRING, %d", 100);
+
+
+  // Test the JOIN message
+  fprintf(stdout, "Testing Joining\n");
+  ret = clacks_new_id(j_id);
+  if (ret == -1) {
+    fprintf(stdout, "Error getting a new ID\n");
+    return -1;
+  } else {
+    clacks_join_id(id, j_id);
+  }
 
   return 0;
 }
